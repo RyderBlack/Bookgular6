4ad159b7dfc63c5c995bddbce9b456faf7b7c961
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-14 13:03:30 +0200

    book service created

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  constructor() { 
+    this.getBooks();
+  }
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  
 }
