febf4cdd8c6c956b21a71fa991d7e31deedfba3e
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-14 12:06:53 +0200

    add firebase to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookgular6';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyA0sbwQDSzrep0Buic-u0Iw-6cRC-xAZ3U",
+      authDomain: "bookgular6.firebaseapp.com",
+      databaseURL: "https://bookgular6.firebaseio.com",
+      projectId: "bookgular6",
+      storageBucket: "bookgular6.appspot.com",
+      messagingSenderId: "883586689322"
+    };
+    firebase.initializeApp(config);
+  }
 }
